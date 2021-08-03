b446a9fab8242f8971381155977d60c15c04edcd
Elliot Robin <mrttasarz@gmail.com>
2021-08-03 23:11:13 +0100

    update

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,15 @@
+
 const config = {
   type: "carousel",
   startAt: 0,
-  perView: 1,
+  perView: 3,
   autoplay: 3000 | true,
-  breakpoints: ({
-    1440: {
-      perView: 3
-    },
-    1024: {
-      perView: 3
-    },
-    768: {
-      perView: 2
-    }
-  })
+  breakpoints: {
+    320: { perView: 1 },
+    480: { perView: 1 },
+    768: { perView: 2 },
+
+  }
 };
 
 new Glide(".glide", config).mount();
