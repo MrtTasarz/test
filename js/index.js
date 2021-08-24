766cf45bcf241177fb32ef95c7c5118c1c86b2f5
Elliot Robin <mrttasarz@gmail.com>
2021-08-24 23:36:54 +0100

    style and lightbox

--- js/index.js
+++ js/index.js
@@ -2,9 +2,6 @@ const menuBtn = document.querySelector('#menu');
 const closeMenu = document.querySelector('#closeMenu');
 const sideNav = document.querySelector('#nav');
 const openFaq = document.querySelectorAll('.openFaq');
-const submitBtn = document.querySelector('#submitBtn');
-const contactSection = document.querySelector('#contact');
-const contactForm = document.querySelector('#contact-form');
 
 const toggleNav = () => {
   sideNav.classList.toggle('active');
@@ -21,24 +18,8 @@ const toggleAnswer = (e) => {
   }
 }
 
-const setAction = (form) => {
-  form.action = "/index.html"
-  showSuccessMessage()
-  return false;
-}
-
-const showSuccessMessage = () => {
-  const message = document.createElement('div')
-  message.classList.add('success-message')
-  message.innerHTML = `<h3>Wiadomość wysłana!<h3>`
-  contact.insertBefore(message, contactForm)
-  setTimeout(() => {
-    contact.removeChild(message)
-  }, 2000)
-}
 
 
 openFaq.forEach(btn => btn.addEventListener('click', (e) => toggleAnswer(e)))
 menuBtn.addEventListener('click', toggleNav)
 closeMenu.addEventListener('click', toggleNav)
-submitBtn.addEventListener('click', showSuccessMessage)
